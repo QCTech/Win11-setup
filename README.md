d8da9dd1859f691acc2357886aecdce557591eca
Gareth Westwood <gareth.westwood@qctech.co.uk>
2022-12-27 16:57:01 +0000

    added initialCleanup.ps1
    Added initialCleanup.ps1 and started to add some descriptions to the readme file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Win11-setup
 Scripts to setup windows 11 just how we like it.
+
+initialCleanup.ps1
+This file will remove all but a couple of the "modern" apps, set the disk and monitor timeouts to a sensible value and disable windows hello.
+
+customise.ps1
+This file will replace the desktop background and logon / lock screen images with one downloaded from a website
+
+Secure.ps1
---
+++ initialCleanup.ps1
@@ -0,0 +1,16 @@
+﻿#### This script will make some changes to the stock windows 11 install ####
+#### It will make it work more like we (QCT) think it should
+
+####Disable passport####
+New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft -Name PassportForWork
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\PassportForWork -Name Enabled -Value 0 -PropertyType DWORD
+
+####Set power####
+powercfg /change monitor-timeout-ac 30
+powercfg /change disk-timeout-ac 30
+powercfg /change standby-timeout-ac 0
+powercfg /change hibernate-timeout-ac 0
+
+####Remove Crap####
+Get-AppxPackage -allusers | where name -notlike "Microsoft.WindowsStore" | where name -notlike "Microsoft.WindowsCalculator" | where name -notlike "Microsoft.MicrosoftStickyNotes" | where name -notlike "Microsoft.Windows.Photos" | Remove-AppPackage
+Get-AppxProvisionedPackage -online | where packagename -notlike "Microsoft.WindowsStore*" | where packagename -notlike "Microsoft.WindowsCalculator*" | where packagename -notlike "Microsoft.MicrosoftStickyNotes*" | where packagename -notlike "Microsoft.Windows.Photos*" | Remove-AppxProvisionedPackage -online
