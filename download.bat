2d73898f45f8a5d46c5374e7c0d5fc5204789e26
Gareth Westwood <gareth@qctech.co.uk>
2022-12-30 13:02:54 +0000

    Fixed a few bits and changed the readme file a bit.

--- download.bat
+++ download.bat
@@ -1,4 +1,4 @@
 @echo off
 REM This batch file will call the powershell script whilst also bypassing the execution policy so you don't have to do that manually.
 REM It just works better than trying to call it by hand.
-powershell.exe -ExecutionPolicy Bypass -File initialCleanup.ps1
+powershell.exe -ExecutionPolicy Bypass -File download.ps1
--- download.ps1
+++ download.ps1
@@ -1,3 +1,23 @@
-Invoke-WebRequest -uri https://raw.githubusercontent.com/QCTech/Win11-setup/master/initialCleanup.ps1  -outfile $baseDirectory\initialCleanup.ps1
+# Set some variables
+$baseDirectory = "C:\qct\"
+
+# Set window title
+$host.ui.RawUI.WindowTitle = "QCT - Windows 11 Setup downloader"
+
+#Check if baseDirectory exists and create it if not
+If ((Test-Path -Path $baseDirectory) -eq $false)
+{
+    Write-Host "File system path does not exist, creating it."
+	New-Item -Path $baseDirectory -ItemType directory
+}
+
+write-host "Downloading initialCleanup.bat."
 Invoke-WebRequest -uri https://raw.githubusercontent.com/QCTech/Win11-setup/master/initialCleanup.bat  -outfile $baseDirectory\initialCleanup.bat
+
+write-host "Downloading initialCleanup.ps1."
+Invoke-WebRequest -uri https://raw.githubusercontent.com/QCTech/Win11-setup/master/initialCleanup.ps1  -outfile $baseDirectory\initialCleanup.ps1
+
+write-host "Downloading customise.ps1."
 Invoke-WebRequest -uri https://raw.githubusercontent.com/QCTech/Win11-setup/master/customise.ps1  -outfile $baseDirectory\customise.ps1
+
+write-host "Please now run the initialCleanup.bat file."
