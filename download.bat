cdc653965d80e65b2a5b168dd52e4a1ea5e34c85
Gareth Westwood <gwes@users.noreply.github.com>
2025-07-06 14:59:51 +0100

    Update download.bat
    Added line to download download.ps1 so that I can keep that updated in real time and so that I only need this one file to bootstrap everything.

--- download.bat
+++ download.bat
@@ -1,4 +1,5 @@
 @echo off
 REM This batch file will call the powershell script whilst also bypassing the execution policy so you don't have to do that manually.
 REM It just works better than trying to call it by hand.
+curl https://raw.githubusercontent.com/QCTech/Win11-setup/master/download.ps1 > download.ps1
 powershell.exe -ExecutionPolicy Bypass -File download.ps1
