4df3ac3105b0345093c4e0eb8f3f4e959c35bd14
Gareth Westwood <gwes@users.noreply.github.com>
2026-06-03 09:54:14 +0100

    Enhance comments in download.bat for clarity
    Updated comments to clarify the purpose of the batch file.

--- download.bat
+++ download.bat
@@ -1,5 +1,8 @@
 @echo off
-REM This batch file will call the powershell script whilst also bypassing the execution policy so you don't have to do that manually.
+REM This batch file is part of QCT's setup process for new machines. It will
+REM Download a powershell script to the current working directory
+REM Call the powershell script whilst also bypassing the execution policy so you don't have to do that manually.
 REM It just works better than trying to call it by hand.
+
 curl https://raw.githubusercontent.com/QCTech/Win11-setup/master/download.ps1 -o download.ps1
-powershell.exe -ExecutionPolicy Bypass -File download.ps1
+powershell.exe -ExecutionPolicy Bypass -File .\download.ps1
