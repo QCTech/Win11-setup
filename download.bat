534a395ef85f152dc6aa20bbdba02d181e4471bc
Gareth Westwood <gwes@users.noreply.github.com>
2026-09-12 12:14:26 +0100

    Enhance download.bat with detailed instructions and checks
    Updated batch file to include detailed echo statements for clarity and improved directory handling.

--- download.bat
+++ download.bat
@@ -1,8 +1,34 @@
 @echo off
-REM This batch file is part of QCT's setup process for new machines. It will
-REM Download a powershell script to the current working directory
-REM Call the powershell script whilst also bypassing the execution policy so you don't have to do that manually.
-REM It just works better than trying to call it by hand.
+echo This batch file is part of QCT's setup process for new machines. It Will
+echo -- create c:\qct if it does not exist
+echo -- move you over to that dir so the next steps are easier
+echo -- delete the current download.ps1 if it exists
+echo -- download a powershell script to the current working directory
+echo -- Call the powershell script whilst also bypassing the execution policy so you don't have to do that manually.
+echo.
+echo It just works better than trying to call it by hand.
+echo.
 
-curl https://raw.githubusercontent.com/QCTech/Win11-setup/master/download.ps1 -o download.ps1
-powershell.exe -ExecutionPolicy Bypass -File .\download.ps1
+REM Check for and create the directory
+if not exist "C:\qct" (
+    echo Dir does not exist, creating it
+    mkdir "C:\qct"
+)
+
+c:
+cd \qct
+
+REM Check for and delete if present the existing download powershell file
+if exist "C:\qct\download.ps1" (
+    echo Existing download.ps1 exists, removing it
+    del "C:\qct\download.ps1"
+)
+
+REM Download the most recent download powershell file from github
+echo Downloading download.ps1 from github
+curl -sS https://raw.githubusercontent.com/QCTech/Win11-setup/master/download.ps1 -o c:\qct\download.ps1
+
+REM Execute the file whilst bypassing the execution policy restrictions
+echo setup done, executing script now
+echo.
+powershell.exe -ExecutionPolicy Bypass -File c:\qct\download.ps1
