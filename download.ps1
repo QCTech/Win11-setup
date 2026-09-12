ffae857b2a7ed6a9a31064eb8cfbbcbec51a0d1c
Gareth Westwood <gwes@users.noreply.github.com>
2026-09-12 11:33:27 +0100

    Add download of tactical.bat and update instructions

--- download.ps1
+++ download.ps1
@@ -11,10 +11,14 @@ If ((Test-Path -Path $baseDirectory) -eq $false)
 	New-Item -Path $baseDirectory -ItemType directory
 }
 
+write-host "Downloading tactical.bat."
+Invoke-WebRequest -uri https://raw.githubusercontent.com/QCTech/Win11-setup/master/tactical.bat  -outfile $baseDirectory\tactical.bat
+
 write-host "Downloading initialCleanup.bat."
 Invoke-WebRequest -uri https://raw.githubusercontent.com/QCTech/Win11-setup/master/initialCleanup.bat  -outfile $baseDirectory\initialCleanup.bat
 
 write-host "Downloading initialCleanup-intune.bat."
 Invoke-WebRequest -uri https://raw.githubusercontent.com/QCTech/Win11-setup/master/initialCleanup-intune.bat  -outfile $baseDirectory\initialCleanup-intune.bat
 
-write-host "Please now run either initialCleanup.bat or initialCleanup-intune.bat file depending on what type of setup the customer has."
+write-host "Please now run tactical.bat followed by either initialCleanup.bat or initialCleanup-intune.bat file depending on what type of setup the customer has."
+write-host "(default to the none intune version as it does all the things)"
